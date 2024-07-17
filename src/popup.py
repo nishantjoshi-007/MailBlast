6b0ea743605cdb6c8b81ee4343ae40e6eb66c941
nishantjoshi@nj26 <144706300+nishantjoshi-007@users.noreply.github.com>
2024-07-17 13:31:38 -0400

    fixed popup

--- src/popup.py
+++ src/popup.py
@@ -16,15 +16,6 @@ def render_modal(st, content):
             st.markdown(
                 f"""
                 <div style="background-color: rgba(0, 0, 0, 0.8); padding: 20px; border-radius: 10px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 1000; max-height: 70vh; overflow-y: auto;">
-                <style>
-                    h3 {
-                        "color: green;"
-                    }
-                    p, span, div, ul, li {
-                        "color: floralwhite;"
-                    }
-                </style>
-                </div>
                     {content}
                 """,
                 unsafe_allow_html=True
