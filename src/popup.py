8d37f1cadd0fb684589f018cb3ceb3afa0a7d73e
nishantjoshi@nj26 <144706300+nishantjoshi-007@users.noreply.github.com>
2024-07-17 14:59:45 -0400

    fixed refresh

--- src/popup.py
+++ src/popup.py
@@ -8,15 +8,11 @@ def hide_modal(st):
 
 # Function to render the modal with content
 def render_modal(st, content):
-    if 'show_modal' not in st.session_state:
-        st.session_state['show_modal'] = False
-
-    if st.session_state['show_modal']:
-        with st.container():
-            st.markdown(
-                f"""
-                <div style="background-color: rgba(0, 0, 0, 0.9); color: floralwhite; padding: 20px; border-radius: 10px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 1000; max-height: 70vh; overflow-y: auto;">
-                    {content}
-                """,
-                unsafe_allow_html=True
-            )
+    with st.container():
+        st.markdown(
+            f"""
+            <div style="background-color: rgba(0, 0, 0, 0.9); color: floralwhite; padding: 20px; border-radius: 10px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 1000; max-height: 70vh; overflow-y: auto;">
+                {content}
+            """,
+            unsafe_allow_html=True
+        )
