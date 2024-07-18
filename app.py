707d9dc8dc5a474ec8e0d4f37c428f2136e6fe67
nishantjoshi@nj26 <144706300+nishantjoshi-007@users.noreply.github.com>
2024-07-18 15:26:02 -0400

    multiple attachements allowed

--- app.py
+++ app.py
@@ -130,21 +130,21 @@ if 'creds' in st.session_state and st.session_state['creds']:
         if 'show_attachments' not in st.session_state:
             st.session_state['show_attachments'] = False
             
-            if st.session_state['attachments']:
-                attach_col1, attach_col2 = st.columns(2)
-                with attach_col1:
-                    if st.button("Show Attachments"):
-                        st.session_state['show_attachments'] = True
-                
-                with attach_col2:       
-                    if st.button("Hide Attachments"):
-                        st.session_state['show_attachments'] = False
+        if st.session_state['attachments']:
+            attach_col1, attach_col2 = st.columns(2)
+            with attach_col1:
+                if st.button("Show Attachments"):
+                    st.session_state['show_attachments'] = True
             
-                if st.session_state['show_attachments']:
-                    for idx, attachment in enumerate(st.session_state['attachments']):
-                        st.write(f"Attachment {idx+1}: {attachment.name}")
-                        utils.attachement_file_type(st, attachment, pdf_viewer, idx, pd)
-                        st.divider()
+            with attach_col2:       
+                if st.button("Hide Attachments"):
+                    st.session_state['show_attachments'] = False
+        
+            if st.session_state['show_attachments']:
+                for idx, attachment in enumerate(st.session_state['attachments']):
+                    st.write(f"Attachment {idx+1}: {attachment.name}")
+                    utils.attachement_file_type(st, attachment, pdf_viewer, idx, pd)
+                    st.divider()
 
         # Allow users to select a predefined template or write their own
         st.session_state['template_option'] = st.selectbox("Select an Email Template", list(templates.PREDEFINED_TEMPLATES.keys()) + ["Custom"], index=None)
